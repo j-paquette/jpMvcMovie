b1649846d90f6571e32ba2bb0c9eb1dcfc0137c6
j-paquette <56076749+j-paquette@users.noreply.github.com>
2024-03-01 16:37:41 -0500

    next step, pass data from controller to view

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -7,14 +7,16 @@ public class HelloWorldController : Controller
 {
     // 
     // GET: /HelloWorld/
-    public string Index()
+    public IActionResult Index()
     {
-        return "This is my default action...";
+        return View();
     }
     // 
     // GET: /HelloWorld/Welcome/ 
-    public string Welcome()
+    // Requires using System.Text.Encodings.Web;
+    public string Welcome(string name, int ID = 1)
     {
-        return "This is the Welcome action method...";
+        //HtmlEncoder.Default.Encode to protect the app from malicious input, such as through JavaScript
+        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
     }
 }
